1612c3ef99b441721f91062576785b51d23e9acf
Malleswar Reddy Kalluru <malleswarreddykalluru@gmail.com>
2024-10-04 19:07:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#import pygame package
+import pygame
+#import the time package to restart the game
+import time
+#import for the random library to randomize the car coming from the opposite side.
+import random
